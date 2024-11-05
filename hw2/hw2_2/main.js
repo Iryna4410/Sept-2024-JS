5ee7d6fa9671029e055dd4a070a029a432dabd14
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-05 15:40:04 +0200

    зробила дз 2_2

---
+++ hw2/hw2_2/main.js
@@ -0,0 +1,19 @@
+
+let book1={
+    title: "451 градус за Фаренгейтом",
+    pageCount:158,
+    genre:"aнтиутопія",
+}
+console.log(book1);
+let book2={
+    title:"Хірург",
+    pageCount:352,
+    genre:"трилер",
+}
+console.log(book2);
+let book3={
+    title:"Витончене мистецтво забивати на все",
+    pageCount:160,
+    genre:"психологія",
+}
+console.log(book3);
