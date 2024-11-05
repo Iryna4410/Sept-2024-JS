c0b1bd00c28993d2b163dc9cf1e3f3f33997d45d
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-05 14:16:59 +0200

    Додала дз 1

---
+++ hw1/hw1_4/main.js
@@ -0,0 +1,9 @@
+let firstName="Ірина";
+let middleName="Василівна";
+let age="21";
+prompt(firstName);
+prompt(middleName);
+prompt(age);
+console.log(firstName);
+console.log(middleName);
+console.log(age);
