85ae855ba5962a4794059da4ac6a9e191d697084
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-07 18:07:49 +0200

    дз 2

---
+++ hw2/hw2_1/main.js
@@ -0,0 +1,25 @@
+let arr;
+arr = [21, "Iryna", "Ivano-Frankivsk", true, 700, 654, 0, false, -315, "34"];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+
+
+
+
+
+
+
+
+
+
+
