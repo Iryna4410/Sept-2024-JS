b7f1e7b8e710450c5f11dfb1d4bf69292788e068
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-07 22:28:50 +0200

    Homework 3

---
+++ hw3/hw3_1/main.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 10;i++) {
+
+    document.write(`<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad animi at aut dignissimos distinctio dolore ducimus eos error eveniet, fuga labore laborum optio pariatur rem sequi ullam vitae voluptate, voluptates voluptatibus.</div>`);
+}
---
+++ hw3/hw3_4/main.js
@@ -0,0 +1,7 @@
+let i = 0;
+
+while( i <20) {
+    document.write(`<h1>Lorem ipsum dolor. ${i}</h1>`)
+    i++;
+
+}
