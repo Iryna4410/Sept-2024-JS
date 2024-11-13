4e05b8a323f4d669fe97eb2257ed16b4dbdbc650
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-13 22:20:12 +0200

    Дз5_8

---
+++ hw5/hw5_8/main.js
@@ -0,0 +1,10 @@
+let myFunction=(array)=>{
+    document.write(`<ul>`)
+    for (const item of array) {
+        document.write(`<li>${item}</li>`)
+     }
+    document.write(`</ul>`)}
+
+   myFunction([12,34,true,"lorem",false])
+
+
