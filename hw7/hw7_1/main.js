b3fff1d8bdcfdfcd31823b98d29a7c5a138e7ae5
Iryna4410 <iryna.kovbasnyuk@gmail.com>
2024-11-18 11:31:43 +0200

    hw7_1

---
+++ hw7/hw7_1/main.js
@@ -0,0 +1,19 @@
+function User(id,name,surname,email, phone) {
+    this.id =id;
+    this.name =name;
+    this.surname =surname;
+    this.email =email;
+    this.phone =phone;
+}
+let arr=[
+user1=new User(1,'vasya','lorem','bjb','09835264657'),
+user2=new User(2,'vasyaa','lorem','bjb','09835264657'),
+user3=new User(3,'anna','lorem','bjb','09835264657'),
+user4=new User(4,'vasya','lorem','bjb','09835264657'),
+user5=new User(5,'vasya','lorem','bjb','09835264657'),
+user6=new User(6,'vasya','lorem','bjb','09835264657'),
+user7=new User(7,'vasya','lorem','bjb','09835264657'),
+user8=new User(8,'vasya','lorem','bjb','09835264657'),
+user9=new User(9,'vasya','lorem','bjb','09835264657'),
+user10=new User(10,'vasya','lorem','bjb','09835264657')]
+console.log(arr);
